1e735f7947ae62ba187b513736506332d6db40c4
hello-william <email+hello+robot@wle8300.com>
2025-12-17 15:33:10 -0800

    Revert "updated SE3 config"
    This reverts commit d6ed808fcc217527f0861fd62f36765afde675eb.

--- launch/SE3_web_interface.launch.py
+++ launch/SE3_web_interface.launch.py
@@ -108,7 +108,7 @@ def generate_launch_description():
                 [
                     teleop_interface_package,
                     "config",
-                    "SE3_configure_video_streams_params.yaml",
+                    "configure_video_streams_params.yaml",
                 ]
             )
         ],
