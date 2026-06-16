68ed54dfd81c15998860b2fd1d371d8470e980f7
Vinitha Ranganeni <vinitha@hello-robot.com>
2026-06-16 12:38:24 -0700

    fixed pre-commit issues

--- src/shared/remoterobot.tsx
+++ src/shared/remoterobot.tsx
@@ -429,7 +429,7 @@ class RobotSensors extends React.Component {
     getOdom(): ROSOdometry | undefined {
         return this.odom;
     }
-    
+
     /**
      * Records a callback from the function provider. The callback is called
      * whenever the battery voltage changes.
